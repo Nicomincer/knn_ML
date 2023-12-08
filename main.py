d1d7d59634216eaa448f98c06223388a68e7c4bf
nicolas <nicolasalencar100@gmail.com>
2023-12-08 14:35:12 -0300

    IA is working

--- main.py
+++ main.py
@@ -3,40 +3,41 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as pt
 import numpy as np 
+from sklearn.model_selection import cross_val_score
 
 #from imblearn.over_sampling import RandomOverSampler
 
 df = pd.read_csv("archive/Iris.csv")
-print(df.shape)
-df.loc[df["Species"] == "Iris-setosa"] = 0 
-df.loc[df["Species"] == "Iris-versicolor"] = 1
-df.loc[df["Species"] == "Iris-virginica"] = 2
-
+print(df.info())
 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
-"""def scale_dataset(dataframe, oversample=False):
-  X = dataframe[dataframe.columns[:-1]].values
-  y = dataframe[dataframe.columns[-1]].values
 
-  scaler = StandardScaler()
-  X = scaler.fit_transform(X)
-
-  if oversample:
-    ros = RandomOverSampler()
-    X, y = ros.fit_resample(X, y)
-
-  data = np.hstack((X, np.reshape(y, (-1, 1))))
-
-  return data, X, y
-
-
-
-train, X_train, y_train = scale_dataset(train, oversample=True)
-valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
-test, X_test, y_test = scale_dataset(test, oversample=False)
-"""
 X_train = df[df.columns[:-1]].values
 y_train = df[df.columns[-1]].values
+X_test = train[train.columns[:-1]].values
+y_test = train[train.columns[-1]].values
+label_map = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+y_train = [label_map[label] for label in y_train]
 knn_model = KNeighborsClassifier(n_neighbors=1)
 knn_model.fit(X_train, y_train)
+
+y_pred = knn_model.predict(X_test)
+print(y_pred)
+a = []
+for valor in y_test:
+    if valor == "Iris-setosa":
+        a.append(0)
+    elif valor == "Iris-versicolor":
+        a.append(1)
+    else:
+        a.append(2)
+print(a)
+contagem = 0
+for c in range(0, len(y_pred)-1):
+    if y_pred[c] == a[c]:
+        contagem+= 1
+
+erros = len(a)-contagem
+acertos = contagem
+print(f"Houve {acertos} acertos e {erros} erros, com rendimento de {(acertos/len(a))*100}%")
