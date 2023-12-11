7ea70464df5cd8503bc2eb198a66a267dfdf1d76
nicolas <nicolasalencar100@gmail.com>
2023-12-11 09:56:23 -0300

    bug solved

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd 
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np 
-from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
 
+#Iris-dataset
+
+#getting the dataset Iris
 df = pd.read_csv("archive/Iris.csv")
 
+#creating the train, valid and test datasets. 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
 
+#Normalizing and creating our vector features. 
 X_train = train[train.columns[:-1]].values
 y_train = train[train.columns[-1]].values
 X_test = test[test.columns[:-1]].values
@@ -16,9 +21,12 @@ label_map = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
 y_train = [label_map[label] for label in y_train]
 y_test = [label_map[label] for label in y_test]
 
+#starting the training
 knn_model = KNeighborsClassifier(n_neighbors=3)
 knn_model.fit(X_train, y_train)
 
+#Our predict
 y_pred1 = knn_model.predict(X_test)
 
-print(classification_report(y_test, y_pred1))
+#Results
+print(precision_score(y_test, y_pred1, pos_label="positive", average="micro"))
